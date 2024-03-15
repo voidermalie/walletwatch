53ffe13dd6ea031877018eb2bdbcc492a93d77eb
voidermalie <lestarfanny@gmail.com>
2024-03-15 15:04:09 +0100

    provider deleted

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { FormProvider } from './utils/context/FormContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormProvider>
       <App />
-    </FormProvider>
   </React.StrictMode>
 );
